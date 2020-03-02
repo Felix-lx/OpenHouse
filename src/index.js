f734f5daef8dd8b1172d04691298a758ef19c184
Felix-lx <417732673@qq.com>
2020-03-02 10:43:10 +0800

    底部提交一次

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import './assets/fonts/iconfont.css'
 import 'antd-mobile/dist/antd-mobile.css'
 // 自己的样式文件写在后面，才能覆盖前面的
 import './index.css'
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,40 +10,15 @@ import Profile from '../Profile'
 export default class Home extends React.Component {
   state = {
     //指定高亮
-    selectedTab: 'redTab',
+    selectedTab: 'blueTab',
     //是否隐藏tabBar
     hidden: false,
     //是否全屏显示
     fullScreen: true,
   }
-  renderContent(pageText) {
-    return (
-      <div style={{ backgroundColor: 'white', height: '100%', textAlign: 'center' }}>
-        <div style={{ paddingTop: 60 }}>Clicked “{pageText}” tab， show “{pageText}” information</div>
-        <a style={{ display: 'block', marginTop: 40, marginBottom: 20, color: '#108ee9' }}
-          onClick={(e) => {
-            e.preventDefault();
-            this.setState({
-              hidden: !this.state.hidden,
-            });
-          }}
-        >
-          Click to show/hide tab-bar
-        </a>
-        <a style={{ display: 'block', marginBottom: 600, color: '#108ee9' }}
-          onClick={(e) => {
-            e.preventDefault();
-            this.setState({
-              fullScreen: !this.state.fullScreen,
-            });
-          }}
-        >
-          Click to switch fullscreen
-        </a>
-      </div>
-    );
-  }
+
   render() {
+    console.log(this.props)
     return (
       <div>
         {/* 变化的上半部分 */}
@@ -56,7 +31,7 @@ export default class Home extends React.Component {
         <div style={this.state.fullScreen ? { position: 'fixed', height: '100%', width: '100%', top: 0 } : { height: 400 }}>
         <TabBar
           unselectedTintColor="#949494"
-          tintColor="#33A3F4"
+          tintColor="#21B97A"
           barTintColor="white"
           hidden={this.state.hidden}
         >
@@ -65,95 +40,106 @@ export default class Home extends React.Component {
             key="Index"
             icon={<div style={{
               width: '22px',
-              height: '22px',
-              background: 'url(https://zos.alipayobjects.com/rmsportal/sifuoDUQdAFKAVcFGROC.svg) center center /  21px 21px no-repeat' }}
+              height: '22px'}}
+              className="iconfont icon-ind"
             />
             }
             selectedIcon={<div style={{
               width: '22px',
-              height: '22px',
-              background: 'url(https://zos.alipayobjects.com/rmsportal/iSrlOTqrKddqbOmlvUfq.svg) center center /  21px 21px no-repeat' }}
+              height: '22px'}}
+              className="iconfont icon-ind"
             />
             }
             selected={this.state.selectedTab === 'blueTab'}
-            badge={10}
             onPress={() => {
+              //切换路由
+              this.props.history.push('/home/index')
               this.setState({
                 selectedTab: 'blueTab',
               });
             }}
-            data-seed="logId"
           >
-            {this.renderContent('Life')}
           </TabBar.Item>
           <TabBar.Item
             icon={
               <div style={{
                 width: '22px',
-                height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/BTSsmHkPsQSPTktcXyTV.svg) center center /  21px 21px no-repeat' }}
+                height: '22px'}}
+                className="iconfont icon-findHouse"
               />
             }
             selectedIcon={
               <div style={{
                 width: '22px',
-                height: '22px',
-                background: 'url(https://gw.alipayobjects.com/zos/rmsportal/ekLecvKBnRazVLXbWOnE.svg) center center /  21px 21px no-repeat' }}
+                height: '22px'}}
+                className="iconfont icon-findHouse"
               />
             }
-            title="Koubei"
-            key="Koubei"
-            badge={'new'}
+            title="找房"
+            key="zhaofang"
             selected={this.state.selectedTab === 'redTab'}
             onPress={() => {
+              //切换路由
+              this.props.history.push('/home/houselist')
               this.setState({
                 selectedTab: 'redTab',
               });
             }}
-            data-seed="logId1"
           >
-            {this.renderContent('Koubei')}
           </TabBar.Item>
           <TabBar.Item
             icon={
               <div style={{
                 width: '22px',
-                height: '22px',
-                background: 'url(https://zos.alipayobjects.com/rmsportal/psUFoAMjkCcjqtUCNPxB.svg) center center /  21px 21px no-repeat' }}
+                height: '22px'}}
+                className="iconfont icon-infom"
               />
             }
             selectedIcon={
               <div style={{
                 width: '22px',
-                height: '22px',
-                background: 'url(https://zos.alipayobjects.com/rmsportal/IIRLrXXrFAhXVdhMWgUI.svg) center center /  21px 21px no-repeat' }}
+                height: '22px'}}
+                className="iconfont icon-infom"
               />
             }
-            title="Friend"
-            key="Friend"
-            dot
+            title="资讯"
+            key="zixun"
             selected={this.state.selectedTab === 'greenTab'}
             onPress={() => {
+              //切换路由
+              this.props.history.push('/home/news')
               this.setState({
                 selectedTab: 'greenTab',
               });
             }}
           >
-            {this.renderContent('Friend')}
           </TabBar.Item>
           <TabBar.Item
-            icon={{ uri: 'https://zos.alipayobjects.com/rmsportal/asJMfBrNqpMMlVpeInPQ.svg' }}
-            selectedIcon={{ uri: 'https://zos.alipayobjects.com/rmsportal/gjpzzcrPMkhfEqgbYvmN.svg' }}
+            icon={              
+              <div style={{
+                width: '22px',
+                height: '22px'}}
+                className="iconfont icon-my"
+              />
+            }
+            selectedIcon={              
+              <div style={{
+                width: '22px',
+                height: '22px'}}
+                className="iconfont icon-my"
+              />
+            }
             title="我的"
             key="Profile"
             selected={this.state.selectedTab === 'yellowTab'}
             onPress={() => {
+              //切换路由
+              this.props.history.push('/home/profile')
               this.setState({
                 selectedTab: 'yellowTab',
               });
             }}
           >
-            {this.renderContent('My')}
           </TabBar.Item>
         </TabBar>
         </div>
